8378f0779fd0c793f66a0a1522caf5fdd9d3aada
ryzen-xp <iron4540@gmail.com>
2026-09-08 09:59:41 +0530

    Implement sq_in_rect function to calculate square sizes from a rectangle; add tests for various cases.

--- src/bin/Rectangle_into_Squares.rs
+++ src/bin/Rectangle_into_Squares.rs
@@ -5,10 +5,45 @@ fn main() {
     println!("Hello World : cargo run --bin Rectangle_into_Squares");
 }
 
-fn sq_in_rect(lng: i32, wdth: i32) -> Option<Vec<i32>> {
-    let total_sqr = lng * wdth;
+fn sq_in_rect(mut lng: i32, mut wdth: i32) -> Option<Vec<i32>> {
 
-    let _max_num = total_sqr.isqrt();
+    if lng == wdth  {
+        return  None;
+    }
 
-    None
+    let  mut vec = Vec::new();
+    
+
+    while lng != wdth {
+
+        if lng > wdth {
+            vec.push(wdth);
+
+            lng -=wdth;
+        }
+
+        else {
+            vec.push(lng);
+            wdth-=lng;
+        }
+        
+    }
+
+    vec.push(lng);
+
+    Some(vec)
+}
+
+
+fn testing(lng: i32, wdth: i32, exp: Option<Vec<i32>>) -> () {
+    assert_eq!(sq_in_rect(lng, wdth), exp)
+}
+
+#[test]
+fn tests_sq_in_rect() {
+
+    testing(5, 3, Some(vec![3, 2, 1, 1]));
+    testing(3, 5, Some(vec![3, 2, 1, 1]));
+    testing(5, 5, None);
+  
 }
